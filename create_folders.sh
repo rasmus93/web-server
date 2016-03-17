0fc1fedb395d6ef83c9208971f80ac6ea6fc5b60
Sergii Balganbaiev <rasmus@yandex-team.ru>
2016-03-17 02:44:28 +0200

    Create gitignore, nginx files.

---
+++ create_folders.sh
@@ -0,0 +1,10 @@
+mkdir -p /home/box/web
+mkdir -p /home/box/web/public
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/uploads
+mkdir -p /home/box/web/etc
+
+cp nginx.conf home/box/web/etc
+cp init.sh home/box/web
