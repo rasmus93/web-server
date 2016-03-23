006a39f5b35479c8343525f520d4da4505808ca1
rasmus93 <rasmuska@ukr.net>
2016-03-23 20:03:26 +0200

    FIx

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ def application(environ, start_response):
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	query = environ.get('QUERY_STRING', '')
 	result = ""
-	for value in qs.split('&'):
+	for value in query.split('&'):
 		result = result + value + '\n'
-	return [result]
+	return result
