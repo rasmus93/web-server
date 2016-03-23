50dd8eb93e86bcc61077331453bffb31a11690df
rasmus93 <rasmuska@ukr.net>
2016-03-23 19:29:14 +0200

    Create start.sh. Fix hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@ def application(environ, start_response):
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	query = environ.get('QUERY_STRING', '')
 	result = "";
-	for value : qs.split('&')
+	for value in qs.split('&'):
 		result = result + value + '\n'
+	return result
---
+++ start.sh
@@ -0,0 +1,27 @@
+mkdir -p home/box/web
+mkdir -p home/box/web/public
+mkdir -p home/box/web/public/img
+mkdir -p home/box/web/public/css
+mkdir -p home/box/web/public/js
+mkdir -p home/box/web/uploads
+mkdir -p home/box/web/etc
+
+cp nginx.conf home/box/web/etc
+cp init.sh home/box/web/etc
+cp hello.py home/box/web
+
+cat > /home/box/web/etc/hello <<EOC
+CONFIG = {
+    'working_dir': '/home/box/web',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=3',
+        '--timeout=60',
+        'hello:application',
+    ),
+}
+EOC
+sudo unlink /etc/gunicorn.d/hello
+sudo ln -s /home/box/web/etc/hello /etc/gunicorn.d/hello
+
+sudo bash /home/box/web/etc/init.sh
